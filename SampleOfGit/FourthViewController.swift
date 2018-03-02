0fe04037ba177ebfb9a94ef9fb20cef1a7fc3585
samrulz <samrulz@samrulzs-Mac.local>
2018-03-03 01:09:28 +0530

    init10

--- SampleOfGit/FourthViewController.swift
+++ SampleOfGit/FourthViewController.swift
@@ -12,7 +12,7 @@ class FourthViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("sandip")
         // Do any additional setup after loading the view.
     }
 
